6d90f7aac96a26e424e77a95c7626db6b70adce5
ardnew <andrew@ardnew.com>
2021-04-16 16:21:08 -0500

    add core install to build script

--- build.sh
+++ build.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
 update-tools() {
-	pushd "${self_dir}" &> /dev/null
-	git submodule sync --recursive
-	git pull --recurse-submodules
+	echo "---"
+	echo "--- UPDATING BUILD SOURCE"
+	echo "---"
+	pushd "${self_dir}" &> /dev/null 
+	git submodule sync --recursive || exit 1
+	git pull --recurse-submodules || exit 2
 	popd &> /dev/null
 }
 
 build-core() {
+	echo "---"
+	echo "--- BUILDING PORTENTA H7 CORE"
+	echo "---"
 	pushd "${platform}" &> /dev/null
-	./mbed-os-to-arduino -a -r "${mbed_path}" "${variant}:${variant}" 
-	./mbed-os-to-arduino -c -r "${mbed_path}" "${variant}:${variant}"
+	./mbed-os-to-arduino -a -r "${mbed_path}" "${variant}:${variant}"
+	./mbed-os-to-arduino -c -r "${mbed_path}" "${variant}:${variant}" || exit 3
+	popd &> /dev/null
+}
+
+install-core() {
+	echo "---"
+	echo "--- INSTALLING PORTENTA H7 CORE"
+	echo "---"
+	push "${platform}" &> /dev/null
+	mkdir -p "variants/${variant}/libs" || exit 4
+	cp -v "${build_path}/${build_name}" "${PWD}/variants/${variant}/libs/${core_name}" || exit 5
 	popd &> /dev/null
 }
 
@@ -18,7 +34,11 @@ self_path=$( readlink -f "${0}" )
 self_dir=$( dirname "${self_path}" )
 platform="${self_dir}/arduino/packages/portenta-tools"
 mbed_path="${self_dir}/mbed/os"
+build_path='/tmp/mbed-os-program/BUILD'
+build_name='mbed-core-PORTENTA_H7_M7.a'
 variant='PORTENTA_H7_M7'
+core_name='libmbed.a'
 
-#update-tools
+update-tools
 build-core
+install-core
