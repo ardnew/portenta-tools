082ebac7044b9c9880cc87b949967bc078149ca7
ardnew <andrew@ardnew.com>
2021-06-07 18:17:31 -0500

    use USB full-speed by default

--- build.sh
+++ build.sh
@@ -1,39 +1,47 @@
 #!/bin/bash
 
 update-tools() {
-	echo "---"
-	echo "--- UPDATING BUILD SOURCE"
-	echo "---"
 	pushd "${self_dir}" &> /dev/null 
-	git submodule sync --recursive || exit 1
-	git pull --recurse-submodules || exit 2
+	git submodule sync --recursive
+	git pull --recurse-submodules
 	rsync -avh --info=progress2 --ignore-existing "${mbed_path}/" "${arduino_mbed_path}/"
 	popd &> /dev/null
 }
 
 build-core() {
-	echo "---"
-	echo "--- BUILDING PORTENTA H7 CORE"
-	echo "---"
 	pushd "${platform}" &> /dev/null
-	rm -rf "${build_path}" || exit 3
+	rm -rf "${build_path}" || exit
+	set +e
 	./mbed-os-to-arduino -a -r "${mbed_path}" "${variant}:${variant}"
-	./mbed-os-to-arduino -c -r "${mbed_path}" "${variant}:${variant}" || exit 4
+	set -e
+	./mbed-os-to-arduino -c -r "${mbed_path}" "${variant}:${variant}"
 	popd &> /dev/null
 }
 
 install-core() {
-	echo "---"
-	echo "--- INSTALLING PORTENTA H7 CORE"
-	echo "---"
-	push "${platform}" &> /dev/null
-	mkdir -p "${platform}/variants/${variant}/libs" || exit 5
-	cp -v "${build_path}/BUILD/${build_name}" "${platform}/variants/${variant}/libs/${core_name}" || exit 6
+	pushd "${platform}" &> /dev/null
+	mkdir -p "${platform}/variants/${variant}/libs"
+	cp "${build_path}/BUILD/${build_name}" "${platform}/variants/${variant}/libs/${core_name}"
+	# install the core in arduino runtime
+	if [[ -d "${arduino_path}" ]]; then
+		pushd "${arduino_path}" &> /dev/null
+		mkdir -p "packages/${self_dir}/hardware/mbed"
+		[[ -e "packages/${self_dir}/tools" ]] ||
+			ln -s "${arduino_path}/packages/arduino/tools" "packages/${self_dir}/"
+		[[ -e "packages/${self_dir}/hardware/mbed/${self_version}" ]] &&
+			rm -rf "packages/${self_dir}/hardware/mbed/${self_version}"
+		ln -s "${platform}" "packages/${self_dir}/hardware/mbed/${self_version}"
+		popd &> /dev/null
+	fi
 	popd &> /dev/null
 }
 
+set -xe
+
 self_path=$( readlink -f "${0}" )
 self_dir=$( dirname "${self_path}" )
+self_version=$( command grep -m 1 '[^[:space:]]' "${self_dir}/VERSION" )
+arduino_path='/usr/local/lib/arduino'
 platform="${self_dir}/arduino/packages/portenta-tools"
 arduino_mbed_path="${platform}/cores/arduino/mbed"
 mbed_path="${self_dir}/mbed/os"
@@ -42,7 +50,8 @@ build_name='mbed-core-PORTENTA_H7_M7.a'
 variant='PORTENTA_H7_M7'
 core_name='libmbed.a'
 
-
 update-tools
 build-core
 install-core
+
+set +xe
