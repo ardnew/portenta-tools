2cf9bacb003959aa9056f31077e27b2aebdb3dc9
ardnew <andrew@ardnew.com>
2021-06-07 17:28:46 -0500

    merge mbed core into arduino prior to patching

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ update-tools() {
 	pushd "${self_dir}" &> /dev/null 
 	git submodule sync --recursive || exit 1
 	git pull --recurse-submodules || exit 2
+	rsync -avh --info=progress2 --ignore-existing "${mbed_path}/" "${arduino_mbed_path}/"
 	popd &> /dev/null
 }
 
@@ -15,8 +16,9 @@ build-core() {
 	echo "--- BUILDING PORTENTA H7 CORE"
 	echo "---"
 	pushd "${platform}" &> /dev/null
+	rm -rf "${build_path}" || exit 3
 	./mbed-os-to-arduino -a -r "${mbed_path}" "${variant}:${variant}"
-	./mbed-os-to-arduino -c -r "${mbed_path}" "${variant}:${variant}" || exit 3
+	./mbed-os-to-arduino -c -r "${mbed_path}" "${variant}:${variant}" || exit 4
 	popd &> /dev/null
 }
 
@@ -25,19 +27,21 @@ install-core() {
 	echo "--- INSTALLING PORTENTA H7 CORE"
 	echo "---"
 	push "${platform}" &> /dev/null
-	mkdir -p "${platform}/variants/${variant}/libs" || exit 4
-	cp -v "${build_path}/${build_name}" "${platform}/variants/${variant}/libs/${core_name}" || exit 5
+	mkdir -p "${platform}/variants/${variant}/libs" || exit 5
+	cp -v "${build_path}/BUILD/${build_name}" "${platform}/variants/${variant}/libs/${core_name}" || exit 6
 	popd &> /dev/null
 }
 
 self_path=$( readlink -f "${0}" )
 self_dir=$( dirname "${self_path}" )
 platform="${self_dir}/arduino/packages/portenta-tools"
+arduino_mbed_path="${platform}/cores/arduino/mbed"
 mbed_path="${self_dir}/mbed/os"
-build_path='/tmp/mbed-os-program/BUILD'
+build_path='/tmp/mbed-os-program'
 build_name='mbed-core-PORTENTA_H7_M7.a'
 variant='PORTENTA_H7_M7'
 core_name='libmbed.a'
+
 
 update-tools
 build-core
