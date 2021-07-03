f05974da41fee8e7c303ee0452a03ce023b1ed10
ardnew <andrew@ardnew.com>
2021-07-02 22:27:11 -0500

    fix core installation paths

--- build.sh
+++ build.sh
@@ -11,9 +11,9 @@ update-tools() {
 build-core() {
 	pushd "${platform}" &> /dev/null
 	rm -rf "${build_path}" || exit
-	set +e
-	./mbed-os-to-arduino -a -r "${mbed_path}" "${variant}:${variant}"
-	set -e
+	#set +e
+	#./mbed-os-to-arduino -a -r "${mbed_path}" "${variant}:${variant}"
+	#set -e
 	./mbed-os-to-arduino -c -r "${mbed_path}" "${variant}:${variant}"
 	popd &> /dev/null
 }
@@ -25,12 +25,15 @@ install-core() {
 	# install the core in arduino runtime
 	if [[ -d "${arduino_path}" ]]; then
 		pushd "${arduino_path}" &> /dev/null
-		mkdir -p "packages/${self_dir}/hardware/mbed"
-		[[ -e "packages/${self_dir}/tools" ]] ||
-			ln -s "${arduino_path}/packages/arduino/tools" "packages/${self_dir}/"
-		[[ -e "packages/${self_dir}/hardware/mbed/${self_version}" ]] &&
-			rm -rf "packages/${self_dir}/hardware/mbed/${self_version}"
-		ln -s "${platform}" "packages/${self_dir}/hardware/mbed/${self_version}"
+		mkdir -p "packages/${self_name}/hardware/mbed"
+		[[ -e "packages/${self_name}/tools" ]] ||
+			ln -s "${arduino_path}/packages/arduino/tools" "packages/${self_name}/"
+		[[ -e "packages/${self_name}/hardware/mbed/${self_version}" ]] &&
+			rm -rf "packages/${self_name}/hardware/mbed/${self_version}"
+		ln -s "${platform}" "packages/${self_name}/hardware/mbed/${self_version}"
+		[[ -e "packages/${self_name}/hardware/mbed/${self_version}/cores/arduino/api" ]] ||
+			ln -s  "../../core-api/api" \
+				"packages/${self_name}/hardware/mbed/${self_version}/cores/arduino/api"
 		popd &> /dev/null
 	fi
 	popd &> /dev/null
@@ -38,16 +41,18 @@ install-core() {
 
 set -xe
 
+arduino_path='/usr/local/lib/arduino'
+variant='PORTENTA_H7_M7'
+
 self_path=$( readlink -f "${0}" )
 self_dir=$( dirname "${self_path}" )
+self_name=$( basename "${self_dir}" )
 self_version=$( command grep -m 1 '[^[:space:]]' "${self_dir}/VERSION" )
-arduino_path='/usr/local/lib/arduino'
-platform="${self_dir}/arduino/packages/portenta-tools"
+platform="${self_dir}/arduino/packages/${self_name}"
 arduino_mbed_path="${platform}/cores/arduino/mbed"
 mbed_path="${self_dir}/mbed/os"
 build_path='/tmp/mbed-os-program'
-build_name='mbed-core-PORTENTA_H7_M7.a'
-variant='PORTENTA_H7_M7'
+build_name="mbed-core-${variant}.a"
 core_name='libmbed.a'
 
 update-tools
